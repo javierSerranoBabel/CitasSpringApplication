ab18bf1824e21b2d9ead19450082f5989e171b1a
Javier Serrano <javier.serrano@babelgroup.com>
2023-03-07 10:24:53 +0100

    Ejecución con varias implementaciones

--- src/main/java/com/helloworld/citasSpring/repository/impl/CitaRepositorySQL.java
+++ src/main/java/com/helloworld/citasSpring/repository/impl/CitaRepositorySQL.java
@@ -4,32 +4,22 @@ import com.helloworld.citasSpring.domain.Cita;
 import com.helloworld.citasSpring.repository.CitaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
 import java.util.List;
 
-@Repository("Memory")
-public class CitaRepositoryMemory implements CitaRepository {
-    private List<Cita> citas;
-
-    public CitaRepositoryMemory() {
-        citas = new ArrayList<>();
-    }
-
-
-
+@Repository("SQL")
+public class CitaRepositorySQL implements CitaRepository {
     @Override
     public void alta(Cita cita) {
-        citas.add(cita);
+
     }
 
     @Override
-    public void baja(int indice) {
-        citas.remove(indice);
+    public void baja(int index) {
 
     }
 
     @Override
     public List<Cita> listar() {
-        return citas;
+        return null;
     }
 }
--- src/main/java/com/helloworld/citasSpring/service/impl/CitaServiceImpl.java
+++ src/main/java/com/helloworld/citasSpring/service/impl/CitaServiceImpl.java
@@ -3,25 +3,31 @@ package com.helloworld.citasSpring.service.impl;
 import com.helloworld.citasSpring.domain.Cita;
 import com.helloworld.citasSpring.repository.CitaRepository;
 import com.helloworld.citasSpring.service.CitaService;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class CitaServiceImpl implements CitaService {
-    CitaRepository citaRepository;
+    private CitaRepository citaRepositoryMemory;
+    private CitaRepository citaRepositorySQL;
 
-    public CitaServiceImpl(CitaRepository citaRepository) {
-        this.citaRepository = citaRepository;
+
+    //Se puede omitir el cuerpo
+    public CitaServiceImpl(@Qualifier("SQL") CitaRepository citaRepositorySQL,@Qualifier("Memory") CitaRepository citaRepositoryMemory) {
+        this.citaRepositoryMemory = citaRepositoryMemory;
+        this.citaRepositorySQL = citaRepositorySQL;
     }
 
+    // if tenemos conexion sql si no memory
     public void alta(Cita cita){
-        citaRepository.alta(cita);
+        citaRepositoryMemory.alta(cita);
     }
     public void baja(int indice){
-        citaRepository.baja(indice);
+        citaRepositoryMemory.baja(indice);
     }
     public List<Cita> listar(){
-        return citaRepository.listar();
+        return citaRepositoryMemory.listar();
     }
 }
